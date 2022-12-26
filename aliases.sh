e5dae4bd9328b726afc9551c9b4ff4c4e5112a6d
Georgy Usatenko <g.usatenko@tinkoff.ru>
2022-12-26 15:52:07 +0300

    fix

--- aliases.sh
+++ aliases.sh
@@ -9,3 +9,28 @@ alias pip='python3 -m pip'
 alias colima_start='sudo echo starting colima &&
                     colima start --cpu 6 --memory 8 --disk 200 --mount $HOME:w &&
                     sudo ln -s $HOME/.colima/docker.sock /var/run/docker.sock'
+
+alias drun='docker-compose run --service-ports'
+
+function dexec {
+  PS=$(docker ps)
+  PS=$(echo "$PS" | grep -E "$1")
+  NUMBER_OF_LINES=$(echo "$PS" | wc -l)
+  if [ $NUMBER_OF_LINES = "1" ]; then
+    C_ID=$(echo "$PS" | sed -E 's/^([0-9a-f]+)(.*)/\1/')
+    docker exec -it "$C_ID" bash
+  else
+    echo "found multiple containers:"
+    echo "$PS"
+    return 1
+  fi
+}
+
+function if_gt_then  {
+  STDIN=$(cat -)
+  if (( $STDIN > $1 )); then
+    echo more
+  else
+    echo less
+  fi
+}
--- main.sh
+++ main.sh
@@ -5,8 +5,4 @@ echo DEFAULT PATH: ${PATH}
 . ~/.bash/settings.sh
 . ~/.bash/aliases.sh
 . ~/.bash/path.sh
-
-if [ -f ~/.git-completion.bash ]; then
-  export GIT_COMPLETION_CHECKOUT_NO_GUESS="1"
-  . ~/.git-completion.bash
-fi
+. ~/.bash/.git-completion.bash
