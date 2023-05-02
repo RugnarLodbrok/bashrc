0f45b09cff5f1802c86fd392c60b58364203c3ba
Georgy Usatenko <g.usatenko@tinkoff.ru>
2023-05-02 11:07:17 +0300

    update aliases

--- aliases.sh
+++ aliases.sh
@@ -14,17 +14,41 @@ alias colima_start='sudo echo starting colima &&
 alias drun='docker-compose run --service-ports'
 alias ddown='docker-compose down'
 alias dup='docker-compose up'
+alias drop_first_line='tail -n +2'
 
 function docker-login-artifactory() {
   dp auth login
   dp auth configure-docker
 }
 
-function dexec {
+function filter_docker_ps {
   PS=$(docker ps)
   PS=$(echo "$PS" | grep -E "$1")
   NUMBER_OF_LINES=$(echo "$PS" | wc -l)
   if [ $NUMBER_OF_LINES = "1" ]; then
+    C_ID=$(echo "$PS" | sed -E 's/^([0-9a-f]+)(.*)/\1/')
+    echo $C_ID
+  else
+    echo "found multiple containers:" >&2
+    echo "$PS" >&2
+    exit 1
+  fi
+}
+
+function dkill {
+  C_ID=$(filter_docker_ps $1)
+  if [[ "$C_ID" != "" ]]; then
+    docker kill "$C_ID"
+  else
+    exit 1
+  fi
+}
+
+function dexec {
+  PS=$(docker ps)
+  PS=$(echo "$PS" | grep -E "$1")
+  NUMBER_OF_LINES=$(echo "$PS" | wc -l | xargs)
+  if [ "$NUMBER_OF_LINES" = "1" ]; then
     C_ID=$(echo "$PS" | sed -E 's/^([0-9a-f]+)(.*)/\1/')
     docker exec -it "$C_ID" "${@:2}"
   else
@@ -48,12 +72,29 @@ function dexec2 {
   fi
 }
 
-
-function if_gt_then  {
+function if_gt_then {
   STDIN=$(cat -)
-  if (( $STDIN > $1 )); then
+  if (($STDIN > $1)); then
     echo more
   else
     echo less
   fi
+}
+
+function docker-rm-all-containers {
+  set -e
+  RUNNING_CIDS=$(docker ps -q)
+  ALL_CIDS=$(docker ps -aq )
+  if [ "$RUNNING_CIDS" ]; then
+    docker stop $RUNNING_CIDS
+  fi
+  if [ "$ALL_CIDS" ]; then
+    docker rm $ALL_CIDS
+  fi
+}
+
+function update_project_docker_image {
+  PREFIX="docker-hosted.artifactory.tcsbank.ru/tmsg"
+  docker pull "$PREFIX/${1}-dev:latest" &&
+    docker tag "$PREFIX/${1}-dev:latest" "${1}_dev:latest"
 }
