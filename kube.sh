745c22118f14c25196efde2bba86267b243c6b94
Georgy Usatenko <g.usatenko@tinkoff.ru>
2024-06-21 12:31:54 +0300

    improve aliases

--- kube.sh
+++ kube.sh
@@ -1,11 +1,21 @@
 alias dp-auth="dp auth login \$(whoami)" # dp auth login also works
-alias kaqa="dp auth configure-kubeconfig --cluster-name ya-ruc1-dev1.dev --account-name \$(whoami)@tinkoff.ru"
-alias kaprod="dp auth configure-kubeconfig --cluster-name ds-prod2.prod --account-name \$(whoami)@tinkoff.ru"
-alias kaprod_m1="dp auth configure-kubeconfig --cluster-name m1-prod2.prod --account-name \$(whoami)@tinkoff.ru"
-alias kaprod_m2="dp auth configure-kubeconfig --cluster-name ix-m2-prod2.prod --account-name \$(whoami)@tinkoff.ru"
-alias k="kubectl"
 
-function kan() {
+function k-activate-cluster() {
+    CLUSTER=$1
+    ME=$(whoami)
+    dp auth configure-kubeconfig --cluster-name "${CLUSTER}" --account-name "$ME"@tinkoff.ru
+}
+
+alias k-activate-cluster-qa='k-activate-cluster ya-ruc1-dev1.dev'
+alias k-activate-cluster-prod='k-activate-cluster ds-prod2.prod'
+alias k-activate-cluster-prod-m1='k-activate-cluster m1-prod2.prod'
+alias k-activate-cluster-prod-m2='k-activate-cluster ix-m2-prod2.prod'
+
+alias k='kubectl'
+
+function k-activate-ns() {
   NS=$(k get ns --output=custom-columns=NAME:.metadata.name | find_entity "$@") || return 1
   k config set-context --current --namespace "$NS"
 }
+
+alias kan='k-activate-ns'
