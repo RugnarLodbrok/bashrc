abfee0215d1fadf14ac345d81c05dd0209a5ed9a
rugnar <rugnarlodbrok@gmail.com>
2025-04-18 12:38:21 +0300

    f

--- howto.sh
+++ howto.sh
@@ -2,7 +2,7 @@
 function ffmpeg-cheatsheet() {
   echo ffmpeg -i input.mp4 output.mp4
   echo ffmpeg -i input.mp4 -vf scale=-1:720 output.mp4
-  echo ffmpeg -i input.mp4 -crf 25 output.mp4  \# quality factor, 0-51 (higher to lower)
-  echo ffmpeg -i input.mp4 -t 00:02:00.0  \# trim duration
-  echo ffmpeg -i input.mp4 -ss 00:00:10.0  \# trim start
+  echo ffmpeg -i input.mp4 -crf 25 output.mp4 $'\t' '# quality factor, 0-51 (higher to lower)'
+  echo ffmpeg -i input.mp4 -t 00:02:00.0 $'\t' '# trim duration'
+  echo ffmpeg -i input.mp4 -ss 00:00:10.0 $'\t' '# trim start'
 }
